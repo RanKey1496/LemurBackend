1d4dc3ed2d8f88e02ee1c0260d46d63c5ecd8762
RanKey1496 <jhongil96@gmail.com>
2017-02-20 22:35:32 -0500

    BD Update

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     hash_secret: 'lemur',
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/lemur'
+	db: process.env.MONGODB || 'mongodb://nomiente:nomiente@ds157349.mlab.com:57349/lemur'
 };
